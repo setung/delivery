c8acd0658c78f1082fe52f774b69fe769dc0c563
DESKTOP-BTTJJGD\4evaj <4evajsh@gmail.com>
2021-10-22 01:58:53 +0900

    @LoginUserId  Argument Resolver 구현

---
+++ src/main/java/setung/delivery/config/WebMvcConfig.java
@@ -0,0 +1,21 @@
+package setung.delivery.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import setung.delivery.argumentresolver.UserIdArgumentResolver;
+
+import java.util.List;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    private final UserIdArgumentResolver userIdArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(userIdArgumentResolver);
+    }
+}
