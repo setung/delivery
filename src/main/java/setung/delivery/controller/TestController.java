2e6d536d0b666269e57194365f50cbde1738209b
setung <4evajsh@gmail.com>
2021-12-21 17:42:20 +0900

    CICD test

---
+++ src/main/java/setung/delivery/controller/TestController.java
@@ -0,0 +1,13 @@
+package setung.delivery.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "test";
+    }
+}
